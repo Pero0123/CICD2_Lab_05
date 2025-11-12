486b088637ecf57b142476f27c911c9f08c546ff
Pero0123 <118348919+Pero0123@users.noreply.github.com>
2025-11-12 18:27:22 +0000

    added crud operations from lab 4

--- app/database.py
+++ app/database.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import OperationalError
+
 # Pick env file by APP_ENV (default dev)
 envfile = {
     "dev": ".env.dev",
